5dd6e3cd53ec28200856a8957678555d988a219c
Roberto Polli <robipolli@gmail.com>
2021-04-11 18:01:44 +0200

    testsuite with tox

---
+++ examples/config.py
@@ -0,0 +1,4 @@
+import os
+
+MY_MOVEHUB_ADD = os.environ['hub']
+MY_BTCTRLR_HCI = 'hci1'
--- examples/test_connection.py
+++ examples/test_connection.py
@@ -1,24 +1,38 @@
-#!/usr/bin/env python3
-
+import os
 from pyb00st.movehub import MoveHub
 from pyb00st.constants import *
-
+import pytest
 from time import sleep
 
-MY_MOVEHUB_ADD = '00:16:53:A4:CD:7E'
-MY_BTCTRLR_HCI = 'hci0'
+MY_MOVEHUB_ADD = os.environ['hub']
+MY_BTCTRLR_HCI = 'hci1'
 
-mymovehub = MoveHub(MY_MOVEHUB_ADD, 'BlueZ', MY_BTCTRLR_HCI)
 
-try:
-    mymovehub.start()
-    mymovehub.subscribe_all()
-    mymovehub.listen_hubtilt(MODE_HUBTILT_BASIC)
+@pytest.fixture
+def hub():
+    return MoveHub(MY_MOVEHUB_ADD, 'BlueZ', MY_BTCTRLR_HCI)
 
-    while True:
-        print('Is connected: ', mymovehub.is_connected())
-        sleep(1)
 
-finally:
-    mymovehub.stop()
+def test_connection(hub):
+    try:
+        hub.start()
+        hub.subscribe_all()
+        hub.listen_hubtilt(MODE_HUBTILT_BASIC)
 
+        for i in range(3):
+            is_connected = hub.is_connected()
+            print('Is connected: ', is_connected)
+            sleep(1)
+    finally:
+        hub.stop()
+
+
+def test_motor_a(hub):
+    raise NotImplementedError
+
+
+def test_motor_b(hub):
+    raise NotImplementedError
+
+def test_motor_x(hub):
+    raise NotImplementedError
